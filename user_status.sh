7d8cd26ba01c00dcb3229d6e3adf3a50fe78652a
Darshan Mistry <51168163+Darshanmistry1995@users.noreply.github.com>
2020-02-02 23:27:54 -0500

    Update user_status.sh

--- user_status.sh
+++ user_status.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Enter username please...."
+echo "Enter username please.."
 read username
 
 echo "Please Enter password"
@@ -9,7 +9,7 @@ read password
 
 if [[ ( $username == "admin" && $password == "admin" ) ]]; then
 
-echo "You  are a valid user...";
+echo "You  are a valid user..";
 else
 
 
