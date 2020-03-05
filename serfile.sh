38af765d69dd5164d0aaebefefcb585d23e0a89f
Darshan Mistry <51168163+Darshanmistry1995@users.noreply.github.com>
2020-03-04 23:07:26 -0500

    Update serfile.sh

--- serfile.sh
+++ serfile.sh
@@ -5,7 +5,6 @@ read -p "Enter a filename: " filename
 if [[ -f $filename ]] ; then
 
     echo "The file $filename exists.."
-    
     read -p "Enter the word you want to find: " word
     grep "$word" "$filename"
  else
