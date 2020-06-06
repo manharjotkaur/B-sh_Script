7cb6fae3fb76c63a266a4878aa48e1674da12756
Darshan Mistry <51168163+Darshanmistry1995@users.noreply.github.com>
2020-06-05 23:40:32 -0400

    Update IF_Bash.sh

--- IF_Bash.sh
+++ IF_Bash.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "Enter te number please...";
+echo "Enter te number please..";
 read n;
 
 if [ $n -lt 10];
 then
 
-echo  "It is a one digit number..."
+echo  "It is a one digit number.."
 else
-echo "It is a  two digit number..."
+echo "It is a  two digit number.."
 fi
